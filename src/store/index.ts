43a80aa696337131ea3d7389b83434044ef0b6f4
Isaac N Reis <isaac.reis@teknisa.com>
2024-12-17 12:26:12 -0300

    first commit

---
+++ src/store/index.ts
@@ -0,0 +1,67 @@
+import http from "@/http";
+import { INotificacao } from "@/interfaces/INotificacao";
+import IProjeto from "@/interfaces/IProjeto";
+import { InjectionKey } from "vue";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import { OBTER_PROJETOS } from "./tipo-acoes";
+import {
+  ADICIONA_PROJETO,
+  ALTERA_PROJETO,
+  DEFINIR_PROJETOS,
+  EXCLUIR_PROJETO,
+  NOTIFICAR,
+} from "./tipo-mutacoes";
+
+interface Estado {
+  projetos: IProjeto[];
+  notificacoes: INotificacao[];
+}
+
+export const key: InjectionKey<Store<Estado>> = Symbol();
+
+export const store = createStore<Estado>({
+  state: {
+    projetos: [],
+    notificacoes: [],
+  },
+  mutations: {
+    [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
+      const projeto = {
+        id: new Date().toISOString(),
+        nome: nomeDoProjeto,
+      } as IProjeto;
+      state.projetos.push(projeto);
+    },
+    [ALTERA_PROJETO](state, projeto: IProjeto) {
+      const index = state.projetos.findIndex((proj) => proj.id == projeto.id);
+      state.projetos[index] = projeto;
+    },
+    [EXCLUIR_PROJETO](state, id: string) {
+      state.projetos = state.projetos.filter((proj) => proj.id != id);
+    },
+    [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
+      state.projetos = projetos;
+    },
+    [NOTIFICAR](state, novaNotificacao: INotificacao) {
+      novaNotificacao.id = new Date().getTime();
+      state.notificacoes.push(novaNotificacao);
+
+      setTimeout(() => {
+        state.notificacoes = state.notificacoes.filter(
+          (notificacao) => notificacao.id != novaNotificacao.id
+        );
+      }, 3000);
+    },
+  },
+  actions: {
+    [OBTER_PROJETOS]({ commit }) {
+      http
+        .get("projetos")
+        .then((response) => commit(DEFINIR_PROJETOS, response.data));
+    },
+  },
+});
+
+export function useStore(): Store<Estado> {
+  return vuexUseStore(key);
+}
